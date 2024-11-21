a39d40704d7e2287782496cf8e9060a08b600c12
Bindu Deekshitha <bindudeekshithadevarapu@gmail.com>
2024-11-21 20:47:16 +0530

    added all the files

---
+++ src/main/java/com/app/javaProjectDemo/Demo/App.java
@@ -0,0 +1,11 @@
+package com.app.javaProjectDemo.Demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My is Bindu Deekshitha. This is my first Maven Project");
+    }
+}
